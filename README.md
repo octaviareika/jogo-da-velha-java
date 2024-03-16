6353d9ad942a09708b497dd217c3614d3b83fee0
octaviareika <ceciliaperet81@gmail.com>
2024-03-15 21:34:04 -0300

    mudando mt coisa no codigo

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 color="blue">Jogo da velha em Java</h2>
+
+## Utilizando jogo da velha em Java por meio da interface gráfica Swing  
+
--- jogoDaVelha.java
+++ jogoDaVelha.java
@@ -1,8 +1,4 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
 import java.awt.*;
-
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -10,158 +6,206 @@ import java.awt.event.ActionEvent; // Import the ActionEvent class
 
 public class jogoDaVelha extends JFrame{
 
-    JButton[][] botoes;
-    private int partidas = 0; // contador de partidas
-    private int situacaoAtual = 1; // situacao do jogo
-    private int turnoDojogador = 1;
-    JLabel contador;
-    JLabel vitoria;
-    JButton resetar; // botao para resetar o jogo
-
+    private JButton[][] botoes;
+    private int jogador = 1;
+    private JButton botaoReiniciar;
+   
+    private int jogadas = 0;
+    private boolean estadoDoJogo = true;
 
     public jogoDaVelha(){
-        super("Jogo da Velha");
-        setSize(600,600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        contador = new JLabel();
-        vitoria = new JLabel();
-        setLayout(new GridLayout(4, 3));
-        resetar = new JButton();
-
+        //JFrame frame = new JFrame();
         
+        this.setSize(400, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar a janela
+        //this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        this.setLayout(new GridLayout(4, 3));
+        this.setTitle("Jogo da Velha");
+
+        JPanel painelBotoes = new JPanel();
+        painelBotoes.setLayout(new GridLayout(3, 3));
+        this.add(painelBotoes);
+
+        botoes = new JButton[3][3]; // instanciando os botoes
         
-        botoes = new JButton[3][3];
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
-                botoes[i][j] = new JButton(); // criando o botao
-                botoes[i][j].setBounds(i * 100, j * 100, 100, 100); // ajustar a posição com base em i e j
+        botaoReiniciar = new JButton("Reiniciar");
+        botaoReiniciar.setSize(30, 50);
 
-                final int finalI = i;
-                final int finalJ = j;
+        JPanel controles = new JPanel();
+        controles.setLayout(new FlowLayout());
+        Dimension botaoSize = new Dimension(100, 150); // Largura 100, Altura 150
 
-                this.add(botoes[i][j]);
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
 
-                botoes[i][j].addActionListener(new ActionListener() { // funcao dentro de funcao
-                    public void actionPerformed(ActionEvent e) {
-                        jogar(finalI, finalJ);
-                        contadorDePartida();
+
+                botoes[i][j] = new JButton();
+                //botoes[i][j].setBounds(i * 150, j * 150, 100, 100); // ajustar a posição com base em i e j
+                // ajusar o tamanho
+                //botoes[i][j].setPreferredSize(new Dimension(100, 100));
+
+                painelBotoes.add(botoes[i][j]);
+
+                final int finalLinha = i;
+                final int finalColuna = j;
+
+
+                botoes[i][j].addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e){
+                       jogar(finalLinha, finalColuna);
                     }
-                    
                 });
             }
         }
 
-        resetar.setText("Resetar");
-        resetar.setFont(new Font("Roboto", Font.PLAIN, 20));
-        resetar.addActionListener(new ActionListener() {
+        this.setVisible(true); // mostrar a janela
+        this.add(controles.add(this.getBotaoReiniciar()));
+        this.getBotaoReiniciar().addActionListener( new ActionListener() {
             public void actionPerformed(ActionEvent e){
-                for (int i =0; i < 3; i ++){
-                    for (int j = 0; j < 3; j++){
-                        botoes[i][j].setText("");
-                        partidas = 0;
-                        situacaoAtual = 1;
-                        turnoDojogador = 1;
-                    }
-                
-                }
+                reiniciarJogo();
             }
+        
         });
-        contador.setText("Partidas: " + String.valueOf(partidas));
-        contador.setFont(new Font("Arial", Font.PLAIN, 20));
-        this.add(contador);
-        this.add(resetar);
-    
+
     }
+
+    // getter e setter
+    public JButton[][] getBotoes(){
+        return this.botoes;
+    }
+
+    public void setBotoes(JButton[][] botoes){
+        this.botoes = botoes;
+    }
+
+    public int getJogador(){
+        return this.jogador;
+    }
+
+    public void setJogador(int jogador){
+        this.jogador = jogador;
+    }
+
+    public int getJogadas(){
+        return this.jogadas;
+    }
+
+    public void setJogadas(int jogadas){
+        this.jogadas = jogadas;
+    }
+
+    public boolean getEstadoDoJogo(){
+        return this.estadoDoJogo;
+    }
+
+    public void setEstadoDoJogo(boolean estadoDoJogo){
+        this.estadoDoJogo = estadoDoJogo;
+    }
+
+    public JButton getBotaoReiniciar(){
+        return this.botaoReiniciar;
+    }
+
+    public void setBotaoReiniciar(JButton botaoReiniciar){
+        this.botaoReiniciar = botaoReiniciar;
+    }
+
+
 
     public void jogar(int i, int j){
-        // verificar se o jogador é X ou O
-        // verificar se a posição está vazia
-        // se estiver vazia, colocar o X ou O
-        // se não estiver vazia, não fazer nada
 
-        while (partidas < 9 && situacaoAtual == 1 && botoes[i][j].getText().equals("")){
-            
-            if (turnoDojogador == 1){
-                //etiqueta.setText("Vez do jogador 1");
-                botoes[i][j].setText("X");
-                botoes[i][j].setFont(new Font("Roboto", Font.PLAIN, 60));
-                turnoDojogador = 2;
-            } else {
-                //etiqueta.setText("Vez do jogador 2");
-                botoes[i][j].setText("O");
-                botoes[i][j].setFont(new Font("Roboto", Font.PLAIN, 60));
-                turnoDojogador = 1;
+        while (this.getEstadoDoJogo() && jogadas < 9 && this.botoes[i][j].getText().equals("")){ // verificar tbm se os botoes estao "em branco"
+            if (jogador == 1){
+                // vez do jogador 1
+                this.botoes[i][j].setText("X");
+                jogador++;
             }
 
+            else { // jogador 2
+                this.botoes[i][j].setText("O");
+                jogador--;
+
+            }
+            // funcao de verificar vitoria aq 
             verificarVitoria();
-            partidas++;
+            incrementarPartida();
         }
+
     }
 
+
+    //verifica Vitoria
     public void verificarVitoria(){
-        // verificar se o jogador 1 ganhou
-        // verificar se o jogador 2 ganhou
-        // verificar se deu velha
 
-        if (partidas == 9 && situacaoAtual == 1){
-            //etiqueta.setText("Deu velha");
+        // olhar empate
+        
+        
+
+        for (int i =0; i < 3; i++){
+            if (botoes[i][0].getText().equals("X") && botoes[i][0].getText().equals(botoes[i][1].getText()) && botoes[i][0].getText().equals(botoes[i][2].getText()) && !botoes[i][0].getText().equals("")){
+                estadoDoJogo = false;
+                JOptionPane.showMessageDialog(null, "Jogador 1 venceu!"); // mensagem de vitoria
+                estadoDoJogo = false;
+            }
+
+            if (botoes[i][0].getText().equals("O") && botoes[i][0].getText().equals(botoes[i][1].getText()) && botoes[i][0].getText().equals(botoes[i][2].getText()) && !botoes[i][0].getText().equals("")){
+
+                estadoDoJogo = false;
+                JOptionPane.showMessageDialog(null, "Jogador 2 venceu!"); // mensagem de vitoria
+                
+            }
+        }
+
+        for (int j =0; j < 3; j++){
+            if (botoes[0][j].getText().equals("X") && botoes[0][j].getText().equals(botoes[1][j].getText()) && botoes[0][j].getText().equals(botoes[2][j].getText()) && !botoes[0][j].getText().equals("")){
+                estadoDoJogo = false;
+                JOptionPane.showMessageDialog(null, "Jogador 1 venceu!"); // mensagem de vitoria
+                estadoDoJogo = false;
+            }
+
+            if (botoes[0][j].getText().equals("O") && botoes[0][j].getText().equals(botoes[1][j].getText()) && botoes[0][j].getText().equals(botoes[2][j].getText()) && !botoes[0][j].getText().equals("")){
+
+                estadoDoJogo = false;
+                JOptionPane.showMessageDialog(null, "Jogador 2 venceu!"); // mensagem de vitoria
+                
+            }
+        }
+
+        // diagonal
+        if (botoes[0][0].getText().equals("X") && botoes[0][0].getText().equals(botoes[1][1].getText()) && botoes[0][0].getText().equals(botoes[2][2].getText()) && !botoes[0][0].getText().equals("")){
+            estadoDoJogo = false;
+            JOptionPane.showMessageDialog(null, "Jogador 1 venceu!"); // mensagem de vitoria
+            estadoDoJogo = false;
+        }
+
+        else if (botoes[0][0].getText().equals("O") && botoes[0][0].getText().equals(botoes[1][1].getText()) && botoes[0][0].getText().equals(botoes[2][2].getText()) && !botoes[0][0].getText().equals("")){
+            estadoDoJogo = false;
+            JOptionPane.showMessageDialog(null, "Jogador 2 venceu!"); // mensagem de vitoria
+            estadoDoJogo = false;
+        }
+
+
+        if (this.getJogadas() == 9){
+            JOptionPane.showMessageDialog(null, "Empate!");
+            estadoDoJogo = false;
+        }
+    }
+
+    public void incrementarPartida(){
+        jogadas++;
+       // System.out.println(jogadas);
+    }
+
+    public void reiniciarJogo(){
+        for (int i =0; i <3; i++){
+            for (int j =0; j <3; j++){
+                botoes[i][j].setText("");
+            }
             
-            situacaoAtual = 0;
         }
-
-        // olhar as diagonais, as linhas e colunas
-        // linhas primeiro
-        for (int i = 0; i < 3; i++){
-            if (this.botoes[i][0].equals(this.botoes[i][1]) && this.botoes[i][1].equals(this.botoes[i][2])){
-                if (this.botoes[i][0].getText().equals("X")){
-                    //etiqueta.setText("Jogador 1 ganhou");
-                    situacaoAtual = 0;
-                } else if (this.botoes[i][0].getText().equals("O")){
-                    //etiqueta.setText("Jogador 2 ganhou");
-                    situacaoAtual = 0;
-                }
-            }
-        }
-
-        ///colunas
-        for (int j = 0; j < 3; j++){
-            if (this.botoes[0][j].getText().equals(this.botoes[1][j].getText()) && this.botoes[1][j].getText().equals(this.botoes[2][j].getText())){
-                if (this.botoes[0][j].getText().equals("X")){
-                    //etiqueta.setText("Jogador 1 ganhou");
-                    situacaoAtual = 0;
-                } else if (this.botoes[0][j].getText().equals("O")){
-                    //etiqueta.setText("Jogador 2 ganhou");
-                    situacaoAtual = 0;
-                }
-
-            }
-        }
-
-
-        if (this.botoes[0][0].getText().equals(this.botoes[1][1].getText()) && this.botoes[1][1].getText().equals(this.botoes[2][2].getText())){
-            if (this.botoes[0][0].getText().equals("X")){
-                //etiqueta.setText("Jogador 1 ganhou");
-                situacaoAtual = 0;
-            } else if (this.botoes[0][0].getText().equals("O")){
-                //etiqueta.setText("Jogador 2 ganhou");
-                situacaoAtual = 0;
-            }
-        }
+        
+        jogadas = 0;
+        estadoDoJogo = true;
     }
-
-    public void incrementaPartida(){
-        partidas++;
-        contadorDePartida();
-    }
-
-    public void contadorDePartida(){
-        this.contador.setText("Partidas: " + partidas );
-    }
-
-
-
-
-
 
 }
